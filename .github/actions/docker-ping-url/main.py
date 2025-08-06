736a23b8546f4cd35dc629eeabbd2c93ddbe53e3
thiha <thiha@localhost.com>
2025-08-06 17:46:30 +0700

    feat(17-2-custom-actions-docker): add custom actions docker workflow

---
+++ .github/actions/docker-ping-url/main.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import time
+
+def ping_url(url, delay, max_trials):
+    trials = 0
+
+    while trials < max_trials:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable")
+                return True
+        except requests.ConnectionError:
+            print(f"Website {url} is unreachable.Retrying in {delay} seconds..")
+            time.sleep(delay)
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL formant: {url}")
+            return False
+
+    return False
+
+def run():
+    website_url = os.getenv("INPUT_URL")
+    delay = os.getenv("INPUT_DELAY")
+    max_trails = int(os.getenv("INPUT_MAX_TRIALS"))
+
+    website_reachable = ping_url(website_url, delay, max_trails)
+
+    if not website_reachable:
+        raise Exception(f"Website {website_url} is unreachable")
+    
+    print(f"Website {website_url} is reachable")
+
+if __name__ == "__main__":
+    run()
